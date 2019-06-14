bc17e7756cdb6745cd8338c32c0a8f096fa13d07
1298743328szyasy <1298743328@qq.com>
2019-06-14 18:32:38 +0800

    实现save方法权限校验

--- src/main/java/cn/imnu/aop/demo1/SpringDemo1.java
+++ src/main/java/cn/imnu/aop/demo1/SpringDemo1.java
@@ -1,4 +1,15 @@
 package cn.imnu.aop.demo1;
 
+import org.junit.Test;
+
 public class SpringDemo1 {
+    @Test
+    public void demo1(){
+        UserDao userDao = new UserDaoImpl();
+        UserDao proxy = (UserDao) new MyJdkProxy(userDao).creatProxy();
+        proxy.save();
+        proxy.delete();
+        proxy.find();
+        proxy.update();
+    }
 }
